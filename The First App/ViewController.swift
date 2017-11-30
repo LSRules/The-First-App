ce4866f5df14d6949a4a414a885b361c42d7adca
Liam Suaza <lsfilms2005@gmail.com>
2017-11-30 17:16:50 +0000

    Initial Commit

---
+++ The First App/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  The First App
+//
+//  Created by Liam on 28/11/2017.
+//  Copyright © 2017 Liam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var count = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    @IBAction func myButton(_ sender: Any) {
+     
+        count = count + 1
+        
+        print(count)
+        
+        if count == 10 {
+            view.backgroundColor = UIColor.green
+            
+            myLabel.text = "You tapped the button 10 times!"
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
