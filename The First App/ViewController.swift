3877d8c5819d96056fdef4795f5abe236ea8f8e5
LSRules <lsfilms2005@gmail.com>
2017-12-01 20:49:52 +0000

    Addition/Subtract choices

--- The First App/ViewController.swift
+++ The First App/ViewController.swift
@@ -10,13 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var count = 0
+//    var count = 0
     
     @IBOutlet weak var myLabel: UILabel!
     
     
+    @IBOutlet weak var topTextField: UITextField!
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+   
+    @IBOutlet weak var arSwitch: UISwitch!
+    
+    @IBOutlet weak var arLabel: UILabel!
+    
     @IBAction func myButton(_ sender: Any) {
-     
+
+        let addition = arSwitch.isOn
+ 
+        if addition{
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        }else{
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+}
+        
+        
+        
+        print(topTextField.text!)
+        print(bottomTextField.text!)
+        
+        /*
         count += 1
         
         print(count)
@@ -26,9 +52,9 @@ class ViewController: UIViewController {
             
             myLabel.text = "You tapped the button 10 times!"
         }
-        
+    */
     }
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
